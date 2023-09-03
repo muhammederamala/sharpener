a90dd7836897518c420af546c7e1068b5306ce80
muhammederamala <muhammederamala15@gmail.com>
2023-09-03 16:13:04 +0530

    modified:   3/main.js

--- 3/main.js
+++ 3/main.js
@@ -1,68 +1,86 @@
 // Function to retrieve existing user data from local storage
 function getUsersFromLocalStorage() {
-    const usersJSON = localStorage.getItem('users');
-    return JSON.parse(usersJSON) || [];
-  }
-  
-  // Function to save user data to local storage
-  function saveUsersToLocalStorage(users) {
-    const usersJSON = JSON.stringify(users);
-    localStorage.setItem('users', usersJSON);
-  }
-  
-  // Function to display users in the DOM
-  function displayUsersInDOM(users) {
-    const userList = document.getElementById('userList');
-    userList.innerHTML = '';
-  
-    users.forEach((user, index) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = `User ${index + 1}: Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}`;
-  
-      // Create a delete button for each user
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = 'Delete';
-      deleteButton.addEventListener('click', function () {
-        deleteUser(index);
-      });
-  
-      listItem.appendChild(deleteButton);
-      userList.appendChild(listItem);
+  const usersJSON = localStorage.getItem('users');
+  return JSON.parse(usersJSON) || [];
+}
+
+// Function to save user data to local storage
+function saveUsersToLocalStorage(users) {
+  const usersJSON = JSON.stringify(users);
+  localStorage.setItem('users', usersJSON);
+}
+
+// Function to display users in the DOM
+function displayUsersInDOM(users) {
+  const userList = document.getElementById('userList');
+  userList.innerHTML = '';
+
+  users.forEach((user, index) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `User ${index + 1}: Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}`;
+
+    // Create an "Edit Email" button for each user
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit Email';
+    editButton.addEventListener('click', function () {
+      editUserEmail(index);
     });
-  }
-  
-  // Function to add a new user
-  function addUser(name, email, phone) {
-    const users = getUsersFromLocalStorage();
-    users.push({ name, email, phone });
-    saveUsersToLocalStorage(users);
-    displayUsersInDOM(users);
-  }
-  
-  // Function to delete a user
-  function deleteUser(index) {
-    const users = getUsersFromLocalStorage();
-    users.splice(index, 1);
-    saveUsersToLocalStorage(users);
-    displayUsersInDOM(users);
-  }
-  
-  // Event listener for form submission
-  document.getElementById('userForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const phone = document.getElementById('phone').value.trim();
-  
-    if (name !== '' && email !== '' && phone !== '') {
-      addUser(name, email, phone);
-      document.getElementById('name').value = '';
-      document.getElementById('email').value = '';
-      document.getElementById('phone').value = '';
-    }
+
+    // Create a delete button for each user
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function () {
+      deleteUser(index);
+    });
+
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    userList.appendChild(listItem);
   });
-  
-  // Initial display of users
-  const initialUsers = getUsersFromLocalStorage();
-  displayUsersInDOM(initialUsers);
-  
+}
+
+// Function to add a new user
+function addUser(name, email, phone) {
+  const users = getUsersFromLocalStorage();
+  users.push({ name, email, phone });
+  saveUsersToLocalStorage(users);
+  displayUsersInDOM(users);
+}
+
+// Function to delete a user
+function deleteUser(index) {
+  const users = getUsersFromLocalStorage();
+  users.splice(index, 1);
+  saveUsersToLocalStorage(users);
+  displayUsersInDOM(users);
+}
+
+// Function to edit a user's email
+function editUserEmail(index) {
+  const users = getUsersFromLocalStorage();
+  const newEmail = prompt('Enter the new email address:');
+  if (newEmail !== null) {
+    users[index].email = newEmail;
+    saveUsersToLocalStorage(users);
+    displayUsersInDOM(users);
+  }
+}
+
+// Event listener for form submission
+document.getElementById('userForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const phone = document.getElementById('phone').value.trim();
+
+  if (name !== '' && email !== '' && phone !== '') {
+    addUser(name, email, phone);
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+  }
+});
+
+// Initial display of users
+const initialUsers = getUsersFromLocalStorage();
+displayUsersInDOM(initialUsers);
