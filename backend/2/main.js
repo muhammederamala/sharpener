8f1b70467a0a8c818f98cde9ddff4135497b34ea
muhammederamala <muhammederamala15@gmail.com>
2023-09-15 20:56:38 +0530

    modified:   backend/2/main.js

--- backend/2/main.js
+++ backend/2/main.js
@@ -1,10 +1,23 @@
-const student = {
-    name: 'max',
-    age: '25',
-    place: 'redbull',
-    greet(){
-        console.log("hi, i am " +this.name)
-    }
-}
+// const student = {
+//     name: 'max',
+//     age: '25',
+//     place: 'redbull',
+//     greet(){
+//         console.log("hi, i am " +this.name)
+//     }
+// }
 
-student.greet()
+// student.greet()
+
+
+
+const fruits = ['apple', 'oranges' , '', 'mango', '' , 'lemon']
+
+console.log(fruits.map(fruit =>{
+    if(fruit===''){
+        return 'empty string'
+    }
+    else{
+        return fruit
+    }
+}))
