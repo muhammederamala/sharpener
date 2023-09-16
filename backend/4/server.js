f3f8dd5013fd3399568705afa729e5588924e705
muhammederamala <muhammederamala15@gmail.com>
2023-09-16 14:49:35 +0530

    modified:   backend/4/server.js

--- backend/4/server.js
+++ backend/4/server.js
@@ -2,7 +2,19 @@ const http = require('http')
 
 
 const server = http.createServer((req,res) =>{
-    console.log('muhammed')
+    if (req.url === '/home') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome home');
+      } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to About Us page');
+      } else if (req.url === '/node') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to my Node.js project');
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+      }
 });
 
 server.listen(4000)
