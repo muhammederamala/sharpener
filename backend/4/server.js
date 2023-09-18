729c817286e1741387b492b97ccc7687371ab86c
muhammederamala <muhammederamala15@gmail.com>
2023-09-18 20:21:08 +0530

    modified:   backend/4/node_modules/.package-lock.json 	modified:   backend/4/node_modules/body-parser/HISTORY.md 	modified:   backend/4/node_modules/body-parser/README.md 	modified:   backend/4/node_modules/body-parser/lib/types/json.js 	modified:   backend/4/node_modules/body-parser/package.json 	new file:   backend/4/node_modules/express/node_modules/body-parser/HISTORY.md 	new file:   backend/4/node_modules/express/node_modules/body-parser/LICENSE 	new file:   backend/4/node_modules/express/node_modules/body-parser/README.md 	new file:   backend/4/node_modules/express/node_modules/body-parser/SECURITY.md 	new file:   backend/4/node_modules/express/node_modules/body-parser/index.js 	new file:   backend/4/node_modules/express/node_modules/body-parser/lib/read.js 	new file:   backend/4/node_modules/express/node_modules/body-parser/lib/types/json.js 	new file:   backend/4/node_modules/express/node_modules/body-parser/lib/types/raw.js 	new file:   backend/4/node_modules/express/node_modules/body-parser/lib/types/text.js 	new file:   backend/4/node_modules/express/node_modules/body-parser/lib/types/urlencoded.js 	new file:   backend/4/node_modules/express/node_modules/body-parser/package.json 	new file:   backend/4/node_modules/express/node_modules/raw-body/HISTORY.md 	new file:   backend/4/node_modules/express/node_modules/raw-body/LICENSE 	new file:   backend/4/node_modules/express/node_modules/raw-body/README.md 	new file:   backend/4/node_modules/express/node_modules/raw-body/SECURITY.md 	new file:   backend/4/node_modules/express/node_modules/raw-body/index.d.ts 	new file:   backend/4/node_modules/express/node_modules/raw-body/index.js 	new file:   backend/4/node_modules/express/node_modules/raw-body/package.json 	modified:   backend/4/node_modules/raw-body/HISTORY.md 	modified:   backend/4/node_modules/raw-body/README.md 	modified:   backend/4/node_modules/raw-body/index.js 	modified:   backend/4/node_modules/raw-body/package.json 	modified:   backend/4/package-lock.json 	modified:   backend/4/package.json 	modified:   backend/4/server.js

--- backend/4/server.js
+++ backend/4/server.js
@@ -1,16 +1,26 @@
 const http = require('http')
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log("In the middleware")
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/',(req, res, next) => {
     next()
 })
 
-app.use((req, res, next) => {
-    console.log("In another middleware")
+app.post('/product', (req,res,next) =>{
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"> <button type="submit">Add product</button> </form>')
+})
+
+app.use('/',(req, res, next) => {
     res.send("<h1>hello, from express </h1>")
 })
 
