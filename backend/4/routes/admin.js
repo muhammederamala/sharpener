ab254bddbf503628513f06e0a16f730d1c16137c
muhammederamala <muhammederamala15@gmail.com>
2023-09-19 20:48:43 +0530

    new file:   19.1 12-serving-files-statically.zip 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/.gitignore 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/app.js 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/package.json 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/public/css/main.css 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/public/css/product.css 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/routes/admin.js 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/routes/shop.js 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/util/path.js 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/views/404.html 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/views/add-product.html 	new file:   19.1 12-serving-files-statically/12-serving-files-statically/views/shop.html 	new file:   19.1 12-serving-files-statically/__MACOSX/._12-serving-files-statically 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/._.gitignore 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/._app.js 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/._package.json 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/._public 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/._routes 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/._util 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/._views 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/public/._css 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/public/css/._main.css 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/public/css/._product.css 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/routes/._admin.js 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/routes/._shop.js 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/util/._path.js 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/views/._404.html 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/views/._add-product.html 	new file:   19.1 12-serving-files-statically/__MACOSX/12-serving-files-statically/views/._shop.html 	modified:   backend/4/routes/admin.js 	modified:   backend/4/routes/shop.js 	modified:   backend/4/server.js 	new file:   backend/4/util/path.js 	new file:   backend/4/views/add-product.html 	new file:   backend/4/views/page-not-found.html 	new file:   backend/4/views/shop.html

--- backend/4/routes/admin.js
+++ backend/4/routes/admin.js
@@ -1,9 +1,13 @@
+const path = require('path')
+
 const express = require('express');
+
+const rootDir = require('../util/path')
 
 const router = express.Router();
 
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><input type="text" name="size"> <button type="submit">Add product</button> </form>')
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
 })
 
 router.post('/product', (req,res,next) =>{
