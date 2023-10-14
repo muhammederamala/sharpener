bd7e7707db6113de43f1aa3077803af0c649fbe4
muhammederamala <muhammederamala15@gmail.com>
2023-10-14 20:54:03 +0530

    renamed:    backend/8/app.js -> backend/8/dist/app.js 	new file:   backend/8/dist/models/todo.js 	new file:   backend/8/dist/routes/todos.js 	renamed:    backend/8/app.ts -> backend/8/src/app.ts 	renamed:    backend/8/models/todo.ts -> backend/8/src/models/todo.ts 	renamed:    backend/8/routes/todos.ts -> backend/8/src/routes/todos.ts 	modified:   backend/8/tsconfig.json

--- backend/8/src/routes/todos.ts
+++ backend/8/src/routes/todos.ts
@@ -2,6 +2,9 @@ import {Router} from 'express';
 
 import { Todo } from '../models/todo'
 
+type RequestBody = { text : string }
+type Requestparams = { todoId : string }
+
 let todos:Todo[] =[]
 
 const router = Router();
@@ -11,9 +14,10 @@ router.get('/',(req,res,next) =>{
 })
 
 router.post('/todo',(req,res,next)=>{
+    const body = req.body as RequestBody
     const newTodo: Todo ={
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     }
 
     todos.push(newTodo)
@@ -22,17 +26,20 @@ router.post('/todo',(req,res,next)=>{
 })
 
 router.put('/todo/:todoId',(req,res,next)=>{
-    const tid = req.params.todoId;
+    const params = req.params as Requestparams
+    const tid = params.todoId;
+    const body = req.body as RequestBody
     const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
     if(todoIndex >=0){
-        todos[todoIndex] = {id:todos[todoIndex].id, text:req.body.text};
+        todos[todoIndex] = {id:todos[todoIndex].id, text:body.text};
         return res.status(200).json({message:"updated todo",todos:todos})
     }
     res.status(404).json({message:"Could not find todo for this ID"})
 })
 
 router.delete('/todo/:todoId',(req,res,next)=>{
-    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    const params = req.params as Requestparams
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId);
     res.status(200).json({message:"deleted",todos:todos})
 })
 
