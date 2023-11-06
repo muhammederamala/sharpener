d35898593f41717beaa088e7b835a3b39340c5b5
muhammederamala <muhammederamala15@gmail.com>
2023-11-06 12:21:43 +0530

    modified:   backend/10/app.js 	modified:   backend/10/controllers/shop.js 	modified:   backend/10/models/user.js 	modified:   backend/10/routes/shop.js 	modified:   backend/10/views/includes/add-to-cart.ejs

--- backend/10/app.js
+++ backend/10/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('65477e8acfa72102546d7341')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name,user.email,user.cart,user._id);
       next();
     })
     .catch(err => console.log(err));
