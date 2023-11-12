8995a3019464188075c5a9bab45ff74e43bbd6c3
muhammederamala <muhammederamala15@gmail.com>
2023-11-12 14:46:50 +0530

    modified:   frontend/2/react-complete-guide/node_modules/.cache/.eslintcache 	modified:   frontend/2/react-complete-guide/node_modules/.cache/default-development/index.pack 	modified:   frontend/2/react-complete-guide/src/components/Expenses/ExpenseItem.js

--- frontend/2/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ frontend/2/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const expenseDate = props.expense.date
-  const expenseTitle = props.expense.title
-  const expenseAmount = props.expense.amount
+  let expenseDate = props.expense.date
+  
 
+  let expenseTitle = props.expense.title
+  const [title,setTitle] = useState(expenseTitle);
   const clickHandler = () =>{
+    setTitle('Updated')
     console.log("Clicked")
+  }
+
+  let expenseAmount = props.expense.amount
+  const [amount,setAmount] = useState(expenseAmount);
+  const addExpenseHandler = () =>{
+    setAmount(100)
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={expenseDate}/>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+        <button onClick={addExpenseHandler}>Change price</button>
       </div>
       <button onClick={clickHandler}>Change title</button>
     </Card>
