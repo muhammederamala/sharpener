ae9de53bad6a6da31463609386ee42f7901afe74
muhammederamala <muhammederamala15@gmail.com>
2023-11-13 12:26:45 +0530

    modified:   frontend/2/react-complete-guide/node_modules/.cache/.eslintcache 	modified:   frontend/2/react-complete-guide/node_modules/.cache/default-development/index.pack 	modified:   frontend/2/react-complete-guide/src/components/NewExpense/ExpenseForm.js

--- frontend/2/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ frontend/2/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -44,15 +44,26 @@ function ExpenseForm() {
     // })
   }
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+    console.log(expenseData)
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
           <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-          <label>Amoubt</label>
+          <label>Amount</label>
           <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
         <div className="new-expense__control">
