4afac7b55e9002af89acf4e6e118e7a812cdf3f8
muhammederamala <muhammederamala15@gmail.com>
2023-11-17 17:34:16 +0530

    modified:   frontend/8/restaurant/src/components/Meals/MealItem/MealItem.js 	modified:   frontend/8/restaurant/src/components/Meals/MealItem/MealItemForm.js 	new file:   frontend/8/restaurant/src/components/Meals/MealItem/MealItemForm.module.css 	new file:   frontend/8/restaurant/src/components/UI/Input.js 	new file:   frontend/8/restaurant/src/components/UI/Input.module.css

--- frontend/8/restaurant/src/components/Meals/MealItem/MealItem.js
+++ frontend/8/restaurant/src/components/Meals/MealItem/MealItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import classes from './MealItem.module.css'
+import MealItemForm from "./MealItemForm";
 
 function MealItem(props) {
   const price = `$${props.price}`;
@@ -12,7 +13,9 @@ function MealItem(props) {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm/>
+        </div>
     </li>
   );
 }
